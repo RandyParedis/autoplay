20b5001c7e7961a7549355f24bffdabdbe9e63f8
Unknown <randy.paredis@student.uantwerpen.be>
2019-01-02 16:11:32 +0100

    .

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ else
     sudo mkdir -p bld; cd bld
     sudo cmake ..
     sudo make
-    ll ../include
+    ls -R ../include
     sudo cp -a ../include/gtest ${GTEST_LOC}/include
     sudo cp -a *.a ${GTEST_LOC}/lib/
     cd ${root}
